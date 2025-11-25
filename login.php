58e54173b7b20e372e0ed9bc34c340e236a7e4ef
Emils S <emils.skabardis@gmail.com>
2025-11-25 14:19:17 +0200

    Update database connection details in login.php
    izmainiju savienojumu lai pieslēdzas pie vm

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "48.222.11.29";
+$username = "example_user";
+$password = "h!JmM3Sygu9Sj!J";
 $dbname = "knn";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -34,3 +34,4 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+
